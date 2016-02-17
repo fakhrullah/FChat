fb253178e39941f38bd529b534d91dfe1b7834e7
fakhrul <fakhrullah@fajarhac.com>
2016-02-17 23:12:50 +0800

    use port from config.js

--- app.js
+++ app.js
@@ -103,7 +103,7 @@ io.on('connection', function (socket) {
     });/**/
 });
 
-http.listen(3000, function () {
+http.listen(config.port, function () {
     console.log('listening on *:3000');
 });
 
